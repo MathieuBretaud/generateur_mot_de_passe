e4d473225718eb049c0b8cf28776836a7c1735ad
MathieuBretaud <mathieu.bretaud@gmail.com>
2022-09-01 17:23:55 +0200

    projet fini

--- Program.cs
+++ Program.cs
@@ -1,53 +1,58 @@
-﻿using System;
+﻿using FormationCs;
+using System;
 
 namespace generateur_mot_de_passe
 {
     class Program
     {
-        static int DemanderNombrePositifNonNul(string question)
-        {
-            return DemanderNombreEntre(question, 1, int.MaxValue);
-        }
-        static int DemanderNombreEntre(string question, int min, int max)
-        {
-            
-             int nombre = DemanderNombre(question);
-             if ((nombre >= min) && (nombre <= max))
-             {
-                return nombre;
-             }
-             Console.WriteLine("ERREUR : Le nombre doit être compris entre " + min + " et " + max);
-
-           return DemanderNombreEntre(question, min, max);
-            
-
-        }
-
-        static int DemanderNombre(string question)
-        {
-           
-            while (true)
-            {
-                Console.Write(question);
-                string reponse = Console.ReadLine();
-                try
-                {
-                    int reponseInt = int.Parse(reponse);
-                    Console.WriteLine("OK");
-                    return reponseInt;
-                }
-                catch
-                {
-                    Console.WriteLine("ERREUR : Vous devez rentrer un nombre");
-                }
-            }
-            
-            
-        }
+        
 
         static void Main(string[] args)
         {
-            int longueurMotDePasse = DemanderNombre("Longueur du mot de passe : ");
+            const int NB_MOTS_DE_PASSE = 10;
+
+            int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
+
+            Console.WriteLine();
+            int choixAlphabet = outils.DemanderNombreEntre("Vous voulez un mot de passe avec : \n" +
+                "1 - Uniquement des caractères en minuscule \n" +
+                "2 - Des caractères minuscules et majuscules \n" +
+                "3 - Des caractères et des chiffres \n" +
+                "4 - Caractère, chiffres et caractères speciaux\n" +
+                "Votre choix : ", 1, 4);
+            
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+            string majuscules = minuscules.ToUpper();
+            string chiffres = "0123456789";
+            string carateresSpeciaux = "#&+-";
+            string alphabet;
+            string motDePasse = "";
+            Random rand = new Random();
+
+            if (choixAlphabet == 1)
+                alphabet = minuscules;
+            else if (choixAlphabet == 2)
+                alphabet = minuscules + majuscules;
+            else if (choixAlphabet == 3)
+                alphabet = minuscules + majuscules + chiffres;
+            else
+                alphabet = minuscules + majuscules + chiffres + carateresSpeciaux;
+
+            int longeurAlphabet = alphabet.Length;
+
+            for (int j = 0; j < NB_MOTS_DE_PASSE; j++)
+            {
+                motDePasse = "";
+                for (int i = 0; i < longueurMotDePasse; i++)
+                {
+                    int index = rand.Next(0, longeurAlphabet);
+                    motDePasse += alphabet[index];
+                }
+
+
+                Console.WriteLine("Mot de passe : " + motDePasse);
+            }
+            
         }
     }
 }
